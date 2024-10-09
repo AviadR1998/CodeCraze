be3a326a7dc26c35aaea810c5aaedbf2f750c7bd
tzlil shkuri <tzlil.shkuri4@gmail.com>
2024-10-09 13:34:27 +0300

    Add new files and updates

--- Forest/fishCount.cs
+++ Forest/fishCount.cs
@@ -10,83 +10,91 @@ public class fishCount : MonoBehaviour
 {
     public GameObject arrow;
     public GameObject target;
-    
+
     public GameObject playerCamera;
 
     public GameObject canvas;
 
     public GameObject endcanvas;
 
+    public GameObject animationCanvas;
+
+
     public TMP_Text explainWorlds;
 
     public TMP_Text explainCode;
 
-    bool flag=false;
+    public TMP_Text animationCode;
+       
 
-    public GameObject []fish;
+    bool flag = false;
 
-    int i =0;
+    public GameObject[] fish;
+
+    int i = 0;
 
     void Start()
     {
-        
+
     }
 
-    private void OnTriggerEnter (Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-           canvas.SetActive(true);
-           Cursor.lockState = CursorLockMode.Confined;
-           Cursor.visible=true;
-           explainWorlds.text = "Meet Count! Count is a special number that loves to keep track of things.\n Imagine Count starts at 0. So, we say: count = 0;\n " +
-           "This means Count is holding the number 0 right now.\n Every time we write count++, Count gets one more.\n It's like giving Count an extra toy to hold! ";
+            canvas.SetActive(true);
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            explainWorlds.text = "Meet Count! Count is a special number that loves to keep track of things.\n Imagine Count starts at 0. So, we say: count = 0;\n " +
+            "This means Count is holding the number 0 right now.\n Every time we write count++, Count gets one more.\n It's like giving Count an extra toy to hold! ";
 
-           explainCode.text = "int main() {.\n \tint fish = 1;  // Starting with 1 fish in the lake.\n \tfish++;  // Now fish is 2 so we have two fish in the lake..\n \tfish++; // Now fish is 3 so we have three fish in the lake..\n \treturn 0;.\n";
+            explainCode.text = "int main() {\n \tint fish = 1;  // Starting with 1 fish in the lake.\n \tfish++;  // Now fish is 2 so we have two fish in the lake..\n \tfish++; // Now fish is 3 so we have three fish in the lake..\n \treturn 0;.\n";
         }
-       
+
     }
     public void ButtonInfoClick()
     {
-        Cursor.lockState = CursorLockMode.Confined;
-        Cursor.visible=true;
         canvas.SetActive(false);
-        flag= true;
+        flag = true;
+        animationCode.text = "Press A and look at the fish in the lake.";
+        animationCanvas.SetActive(true);
+
     }
 
-    
+
     void Update()
     {
-       if (arrow.active)
-       {
-        arrow.transform.position = Vector3.MoveTowards(arrow.transform.position,
-            GameObject.Find("FirstPersonController").transform.position + new Vector3(
-            playerCamera.transform.forward.x * 5,
-            math.sin(playerCamera.transform.forward.y) * 5 + 2.7f,
-            playerCamera.transform.forward.z * 5), 6 * Time.deltaTime);
-        arrow.transform.LookAt(target.transform); 
-       }
-       
-       if (flag == true)
-       {
-       {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                if (i <= 3)
+        if (arrow.active)
+        {
+            arrow.transform.position = Vector3.MoveTowards(arrow.transform.position,
+                GameObject.Find("FirstPersonController").transform.position + new Vector3(
+                playerCamera.transform.forward.x * 5,
+                math.sin(playerCamera.transform.forward.y) * 5 + 2.7f,
+                playerCamera.transform.forward.z * 5), 6 * Time.deltaTime);
+            arrow.transform.LookAt(target.transform);
+        }
+
+        if (flag == true)
+        {
+                if (Input.GetKeyDown(KeyCode.A))
                 {
-                    fish[i].SetActive(true);
-                    i++;
+                    if (i <= 3)
+                    {
+                        fish[i].SetActive(true);
+                        i++;
+                    }
                 }
+        }
+        if (i == 4)
+        {
+            animationCanvas.SetActive(false);
+            endcanvas.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                endcanvas.SetActive(false);
+                i++;
             }
         }
-       }
-       if (i == 4)
-       {
-        print("ZZZZ");
-        endcanvas.SetActive(true);
-        //well done now there is 5 fish in the magic lake!
 
-       }
-      
     }
 }
